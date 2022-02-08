373fa24ab853cd9e9410249eca2810caffc3a128
Bünyamin Kırmızı <55808189+bunyaminkirmizi@users.noreply.github.com>
2022-02-08 23:51:45 +0300

    update
    unnecessary comments removed and main website added

--- moore_machine/main.js
+++ moore_machine/main.js
@@ -1,24 +1,27 @@
 class Edge{
-    constructor(in_val,out_val,out_node){
+    constructor(in_val,out_node){
         this.in_val = in_val;
-        this.out_val = out_val;
         this.out_node = out_node;
     }
     get_next_node(){
         return this.out_node
     }
-    get_out_value(){
-        return this.out_val
-    }
 }
 
 class Node{
-    constructor(index) {
-    this.state_num = index
-      this.edges = [];
+    constructor(index,out) {
+    this.state_num = index;
+    this.out = out;
+    this.edges= new Array();
     }
     set_edge(edge) {
         this.edges.push(edge)
+    }
+    set_output(out){
+        this.out = out;        
+    }
+    get_out_value(){
+        return this.out;
     }
     get_edge(for_input){
         let el = null
@@ -59,7 +62,8 @@ class Machine{
                 const out_state = this.state[next]
 
                 console.log(`${index} nodeuna giris ${input} ise cikis ${output} ve git: ${next}`);
-                const edge = new Edge(input,output,out_state)
+                const edge = new Edge(input,out_state)
+                this.state[index].set_output(output)
                 this.state[index].set_edge(edge)
             }
         }
@@ -73,7 +77,7 @@ class Machine{
             if(this.input_alphabet.includes(element)){
                 console.log(`${element} validation check ok`)
             }else{
-                return `Invalid values: Your alphabet = {${this.input_alphabet}} please enter valid values`
+                return `Invalid values: Your alphabet = ${this.input_alphabet} please enter valid values`
             }
         }
 
@@ -85,24 +89,41 @@ class Machine{
 
             for (let index = 0; index < this.state.length; index++) {
                 const e = this.state[index];
+                if(index == 0){
+                    document.getElementById(`q${index}`).className="bg-primary"
+                }else{
                     document.getElementById(`q${index}`).className=""
+                }
+                    
             }
-            document.getElementById(`q0`).className="bg-primary"
 
             return `Enter values from alphabet = ${this.input_alphabet} your output will be here`;
         }
 
+        document.getElementById('q0').className="bg-primary"
+
         input.forEach(element => {
+
+            output += iter_state.out
+            for (let index = 0; index < this.input_alphabet.length; index++) {
+                const e = this.input_alphabet[index];
+                if(e != element){
+                    document.getElementById(`out-${e}`).className=""
+                }
+            }
+
+            
+            document.getElementById(`out-${element}`).className="bg-danger"
 
             let edgetonext = iter_state.get_edge(element)
             if(edgetonext == null){
                 console.log("ERROR INPUT VALUES NOT IN ALPHABET",iter_state,element,edgetonext);
                 return "ERROR INPUT VALUES NOT IN ALPHABET"
             }
+
             
-            output += edgetonext.out_val
             iter_state = edgetonext.out_node
-            
+
             for (let index = 0; index < this.state.length; index++) {
                 const e = this.state[index];
                 if(e != iter_state){
@@ -111,26 +132,25 @@ class Machine{
                     document.getElementById(`q${index}`).className="bg-primary"
                 }
             }
-
-            for (let index = 0; index < this.input_alphabet.length; index++) {
-                const e = this.input_alphabet[index];
-                if(e != element){
-                    document.getElementById(`out-${e}`).className=""
-                }else{
-                    document.getElementById(`out-${element}`).className="bg-danger"
-                }
-            }
         });
 
-        return output
+        return output+iter_state.out
     }
   }
 
-mealey_machine=null
+class Page{
+    constructor(){
+
+    }
+}
 number_of_states = 0
+
 input_alphabet = []
 output_alphabet = []
-transition_table = {}
+
+transition_table = {
+
+}
 
 /*
 Transition table format
@@ -148,6 +168,7 @@ Transition table format
     ...
 }
 */
+moore_machine=null
 
 function gen_table_row(args) {
     let row = '<tr>'
@@ -211,14 +232,12 @@ function gen_table_option_output(table_name,number_of_states,alphabet,alp_output
     <thead class="table-dark">
         <th class="text-center" scope="col" colspan="100">${table_name}</th>
     </thead>
-    ${gen_table_head_elements(["STATES",...alphabet])}
+    ${gen_table_head_elements(["STATES","OUTPUT"])}
     <tbody>`
     for (let index = 0; index < number_of_states; index++) {
         let optable = new Array()
-        for (let j = 0; j < alphabet.length; j++) {
-            let id = `O${index}${alphabet[j]}`
+            let id = `O${index}`
             optable.push(gen_option_menu(alp_output,id))
-        }
         table+=gen_table_row([index,...optable])
     }   
     
@@ -231,8 +250,8 @@ function build_transition_table(number_of_states,input_alphabet){
         transition_table_json[index] = null
         input_json = {}
         input_alphabet.forEach(element => {
-            let output_value = String(document.getElementById(`O${index}${element}`).value)
-            console.log(document.getElementById(`O${index}${element}`).value)
+            let output_value = String(document.getElementById(`O${index}`).value)
+            console.log(document.getElementById(`O${index}`).value)
             let output_state = parseInt(document.getElementById(`T${index}${element}`).value)
             input_json[element] = [output_value,output_state]
         })
@@ -242,12 +261,12 @@ function build_transition_table(number_of_states,input_alphabet){
 }
 function change_transition_table(){
     transition_table = build_transition_table(number_of_states,input_alphabet)
-    mealey_machine = new Machine(number_of_states,
+    moore_machine = new Machine(number_of_states,
         input_alphabet,
         output_alphabet,
         transition_table)
     
-    mealey_machine.build()
+    moore_machine.build()
 }
 function test (){
     newtable = gen_table_option("TRANSITION TABLE",number_of_states,input_alphabet)
@@ -260,6 +279,7 @@ function test (){
 test()
 input = document.getElementById("inputState")
 input.addEventListener('input', updateValue);
+
 
 function updateValue(e) {
     console.log(e.target.value)
@@ -293,11 +313,11 @@ function updateValue(e) {
       function updateValue4(e) {
         console.log(e.target.value)
         
-        document.getElementById("runOutput").value = mealey_machine.run(e.target.value)
+        document.getElementById("runOutput").value = moore_machine.run(e.target.value)
       }
 function build_machine_and_show_transition_table(){
     transition_table = build_transition_table(number_of_states,input_alphabet)
-    mealey_machine = new Machine(number_of_states,input_alphabet,output_alphabet,transition_table)
+    moore_machine = new Machine(number_of_states,input_alphabet,output_alphabet,transition_table)
     let run = `          <form class="row g-3">
     <div class="col-12">
       <label for="inputPassword2" class="visually-hidden"></label>
@@ -305,7 +325,7 @@ function build_machine_and_show_transition_table(){
         type="text"
         class="form-control text-center"
         id="inputRun"
-        placeholder="Input for machine e.g. 010101 "
+        placeholder="Input for machine like 010101 "
       />
     </div>
     <div class="col-12">
@@ -319,55 +339,57 @@ function build_machine_and_show_transition_table(){
       />
     </div>`
     document.getElementById("main").innerHTML = run + generate_tt_run()
-    document.getElementById(`q0`).className="bg-primary"
-
+    moore_machine.run("")
     input4 = document.getElementById("inputRun")
     input4.addEventListener('input', updateValue4);
 }
 
 function generate_tt_run(){
-    headinput = ''
+    headinput = `<th scope="col">OLD STATE</th>`
     new_state_output = '<th scope="col">OUTPUT</th><th scope="col">NEW STATE</th>'.repeat(input_alphabet.length)
     input_alphabet.forEach(element => {
-        headinput += `<th id="out-${element}" scope="col" colspan=2>${element}</th>`
+        
+        headinput += `<th id="out-${element}" scope="col">After Input  ${element}</th>`
     });
+    headinput += `<th scope="col">Character Printed</th>`
+
     let tr_elements =''
     for (let index = 0; index < this.number_of_states; index++) {
         const element = this.transition_table[index];
         tr_elements += `<tr id="q${index}">
         <th scope="row">q${index}</th>
         `
+        let output = ''
         for (let jindex = 0; jindex < this.input_alphabet.length; jindex++) {
             const input = this.input_alphabet[jindex]
-            const output = element[input][0]
+            output = element[input][0]
             const next = element[input][1]
             console.log(tr_elements)
             tr_elements += `
-            <td>${output}</td>
-            <td>q${next}</td>`
-          ;
+            <td>q${next}</td>`;
         }
+        tr_elements += `<td>${output}</td>`
         tr_elements += `</tr>`
     }
-    let tt =`<table class="table text-center table-bordered">
+    let tt =`<table class="table table-bordered text-center">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col" colspan="100">Transition Table and Output Table</th>
+      </tr>
+    </thead>
     <thead>
       <tr>
-        <th scope="col">#</th>
         ${headinput}
       </tr>
-      <tr>
-        <th scope="col">OLD STATE</th>
-        ${new_state_output}
-      </tr>
+
     </thead>
     <tbody>
         ${tr_elements}
     </tbody>
-  </table>
-  <small class="text-primary">&#x25A0; blue color stands for current state<br>
-</small>
-<small class="text-danger">&#x25A0; red color stands for last input that you've entered<br>
-</small>`
+  </table>  <small class="text-primary">&#x25A0; blue color stands for current state<br>
+  </small>
+  <small class="text-danger">&#x25A0; red color stands for last input that you've entered<br>
+  </small>`
 
   return tt
 }
